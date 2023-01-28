e74b2f37a1a9f9fd6256a072ce41c46af187ea66
Felizberto Macedo <55799002+fmacedo7@users.noreply.github.com>
2023-01-27 23:19:41 -0300

    Create scale.cpp

---
+++ scale.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+void drawTriangle() {
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-0.5f, -0.5f, 0.0f);
+    glVertex3f(0.5f, -0.5f, 0.0f);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glEnd();
+}
+
+void buttonCallback(GLFWwindow* window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        // Scale the triangle
+        glScalef(1.5f, 1.5f, 1.0f);
+    }
+}
+
+int main() {
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Triangle and button", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Set the mouse button callback function */
+    glfwSetMouseButtonCallback(window, buttonCallback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        drawTriangle();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
